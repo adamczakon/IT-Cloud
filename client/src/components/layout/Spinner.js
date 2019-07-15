cd91757ad2be5f28800403094894f52cd715a7ec
Adam Czakon <adam.czakon63@gmail.com>
2019-07-15 13:24:16 +0200

    Deployment

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -1,8 +1,5 @@
-import React, { Fragment } from "react";
-import spinner from "./spinner.gif";
+import React from "react";
 
-export default () => (
-  <Fragment>
-    <img src={spinner} style={{ width: "200px", margin: "auto", display: "block" }} alt='Loading...' />
-  </Fragment>
-);
+export default function Spinner() {
+  return <div class='loader'>Loading...</div>;
+}
